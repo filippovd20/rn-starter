762009a7b7e456973656d75018b25c48bb0124b3
Dmitry Filippov <filippovd20@gmail.com>
2020-06-21 23:05:03 +0200

    41 - Notes on State

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,24 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Counter Count:</Text>
+      <Text>Counter Count:{counter}</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter(counter+1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter-1);
+        }}
+      />
     </View>
   );
 };
