fce431b506d99831794c0455688ed7f00e16ed13
Dmitry Filippov <filippovd20@gmail.com>
2020-06-21 22:50:13 +0200

    40 - State in action

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const CounterScreen = () => {
+  return (
+    <View>
+      <Text>Counter Count:</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
