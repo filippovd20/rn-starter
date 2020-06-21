07736ade01fc966499fbfe5b16856b66997863ac
Dmitry Filippov <filippovd20@gmail.com>
2020-06-21 21:53:32 +0200

    28 Destructuring Props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <View>
-    <Text style={styles.text}>hello world</Text>
-    <Button 
-      title="Go to Components Demo"
-      onPress={()=>console.log('Button pressed')}
-    />
-    <TouchableOpacity onPress={()=>console.log('List Pressed')}>
-      <Text>Got to List Demo</Text>
-    </TouchableOpacity>
-  </View>
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Menu</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => navigation.navigate("Component")}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
