ebec84348f2578c7e9ef58793465c2dfdbc9ddfc
Dmitry Filippov <filippovd20@gmail.com>
2020-06-25 22:40:12 +0200

    Validating state changes

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
@@ -10,20 +12,21 @@ const SquareScreen = () => {
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setGreen(red + 1)}
-        onDecrease={() => setGreen(red - 1)}
+        onIncrease={() => setGreen(red + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(red - COLOR_INCREMENT)}
         color="Green"
       />
       <ColorCounter
-        onIncrease={() => setBlue(red + 1)}
-        onDecrease={() => setBlue(red - 1)}
+        onIncrease={() => setBlue(red + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(red - COLOR_INCREMENT)}
         color="Blue"
       />
+      <View style={{height:150, width:150, backgroundColor: `rgb(${red},${green},${blue})`}} />
     </View>
   );
 };
