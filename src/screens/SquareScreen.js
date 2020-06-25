60c7c22963a47035d673ad11784d9976012cd191
Dmitry Filippov <filippovd20@gmail.com>
2020-06-25 23:14:34 +0200

    52 - Reusable State updates

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -10,37 +10,55 @@ const SquareScreen = () => {
   const [blue, setBlue] = useState(0);
 
   const setColor = (color, change) => {
-      // color === 'red', 'green', 'blue'
-      // change === +15 , -15 
+    // color === 'red', 'green', 'blue'
+    // change === +15 , -15
 
-      if (color === 'red') {
-        if (red+change >255 || red+change <0) {
-          return;
-        } else {
-          setRed(red+change);
-        }
-      };
-
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+      default:
+        return;
+    }
   };
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setColor('red', COLOR_INCREMENT)}
-        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
+        onIncrease={() => setColor("red", COLOR_INCREMENT)}
+        onDecrease={() => setColor("red", -1 * COLOR_INCREMENT)}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setGreen(red + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(red - COLOR_INCREMENT)}
+        onIncrease={() => setColor("green", COLOR_INCREMENT)}
+        onDecrease={() => setColor("green", -1 * COLOR_INCREMENT)}
         color="Green"
       />
       <ColorCounter
-        onIncrease={() => setBlue(red + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(red - COLOR_INCREMENT)}
+        onIncrease={() => setColor("blue", COLOR_INCREMENT)}
+        onDecrease={() => setColor("blue", -1 * COLOR_INCREMENT)}
         color="Blue"
       />
-      <View style={{height:150, width:150, backgroundColor: `rgb(${red},${green},${blue})`}} />
+      <Text>
+        {red} - {green} - {blue}
+      </Text>
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green},${blue})`,
+        }}
+      />
     </View>
   );
 };
