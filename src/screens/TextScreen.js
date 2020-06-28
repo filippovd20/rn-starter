456572fca9005ca49b87aaaa1b77faae625de8ad
Dmitry Filippov <filippovd20@gmail.com>
2020-06-28 19:16:58 +0200

    64 - text state

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
+  const [name, setName] = useState("");
+
   return (
     <View>
-      <TextInput style={styles.input}/>
+      <Text>Enter Name:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      <Text>MY name is {name}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    input: {
-        margin: 15,
-        borderColor: 'black',
-        borderWidth: 1
-    }
+  input: {
+    margin: 30,
+    borderColor: "black",
+    borderWidth: 1,
+  },
 });
 
 export default TextScreen;
