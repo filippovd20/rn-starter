2faf462e9e1a292378c50f60957116cd03b905f1
Dmitry Filippov <filippovd20@gmail.com>
2020-06-28 20:07:32 +0200

    73 - AlignSelf

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,26 +4,34 @@ import { View, Text, StyleSheet } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child #1</Text>
-      <Text style={styles.textStyle}>Child #2</Text>
-      <Text style={styles.textStyle}>Child #3</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-   viewStyle: {
-    borderWidth:3,
-    borderColor:'black',
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: "black",
     height: 200,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-around'
-   },
-   textStyle:{
-       borderWidth: 3,
-       borderColor: 'red',
-   }
+  },
+  textOneStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    flex: 4
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    flex: 4
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    flex: 2
+  },
 });
 
 export default BoxScreen;
